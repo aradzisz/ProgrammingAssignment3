93de01d2121110b8d4d9ff64cd2716d8684c5f04
aradzisz@github.com <aradzisz@github.com>
2016-01-28 20:06:30 +0100

    Removed characters from vector used to get min. Working properly

--- best.R
+++ best.R
@@ -1,8 +1,10 @@
 best <- function(state, outcome) {
   outcome_data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  #check if state is correct
   if (!(state %in% unique(outcome_data$State))) {
     stop("invalid state")
   }
+  #check if outcome passed as an argument is correct. if yes, substract hospital name and outcome value
   if (outcome == "heart attack") {
     state_outcome_data <- outcome_data[outcome_data$State == state,c(2, 11)]
   } else if (outcome == "heart failure") {
@@ -12,5 +14,8 @@ best <- function(state, outcome) {
   } else {
     stop("invalid outcome")
   }
-  state_outcome_data[state_outcome_data[,2] == min(state_outcome_data[,2], na.rm = TRUE),1]
+  #remove non - numeric values
+  state_outcome_data <- state_outcome_data[!is.na(suppressWarnings(as.numeric(state_outcome_data[,2]))),]
+  #return row where second column value is minimal
+  state_outcome_data[as.numeric(state_outcome_data[,2]) == min(as.numeric(state_outcome_data[,2])),1]
 }
